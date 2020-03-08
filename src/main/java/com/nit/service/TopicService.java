fbb91efa7223ac952ab4996aff285f7b3c2571c2
pavan kumar <pavankumar@scmgalaxy.com>
2020-03-07 20:17:33 -0500

    creating a new resource/topic using POST request

--- src/main/java/com/nit/service/TopicService.java
+++ src/main/java/com/nit/service/TopicService.java
@@ -1,5 +1,6 @@
 package com.nit.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,12 +11,16 @@ import com.nit.model.Topic;
 @Service
 public class TopicService {
 
-	private List<Topic> topics = Arrays.asList(
+	//contains a List of Topics
+	//Created a List of hard coded values using Arrays.asList... Arrays.asList method creates an Immutable list
+	//In order to edit the List we converted to new ArrayList<>( )...
+	private List<Topic> topics = new ArrayList<>( Arrays.asList(
 			new Topic("spring", "spring framework", "spring framework Description"),
 			new Topic("java", "core java", "core java Description"),
 			new Topic("javascript", "javascript", "javascript Description")
-		); 
+		)); 
 	
+	//Get All the Topics
 	public List<Topic> getAllTopics(){
 		return topics;
 	}
@@ -24,5 +29,11 @@ public class TopicService {
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
 	}
+
+	//Creating a new Topic
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+	
 	
 }
